e03b27188864179b6f1952fec014bc9a3fcf0f19
gitshubhsss <shubhamranjane16@gmail.com>
2024-05-25 22:15:06 +0530

    Find out the largest from the array

---
+++ Array/largest.js
@@ -0,0 +1,10 @@
+//find the largest number from the array only positive number
+
+let arr=[5,8,2,8,2,85,685,685,8];
+let large=0;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]>large){
+        large=arr[i];
+    }
+}
+console.log(large);
