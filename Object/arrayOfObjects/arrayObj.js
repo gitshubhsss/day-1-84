1050f13c2f0c86429330057d9f100403f1c7e3f9
gitshubhsss <shubhamranjane16@gmail.com>
2024-05-25 22:15:51 +0530

    array of objects

---
+++ Object/arrayOfObjects/arrayObj.js
@@ -0,0 +1,18 @@
+const classInfo = [
+  {
+    name: "aman",
+    grade: "A+",
+    city: "delhi",
+  },
+  {
+    name: "ritik",
+    grade: "A",
+    city: "pune",
+  },
+  {
+    name: "kartik",
+    grade: "O",
+    city: "Rohini",
+  },
+];
+console.log(classInfo[0].city);
---
+++ Object/arrayOfObjects/obj.js
@@ -0,0 +1,12 @@
+let student;
+let arr=[
+    student={
+        name:"akshy",
+        age:58
+    },
+    teacher={
+        name:"aditi mam",
+        class:"sy msc"
+    }
+];
+console.log(arr[]);
